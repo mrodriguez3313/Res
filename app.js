f0951b33eb576b3498a1c6f807e8948dec31dc22
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-05-08 19:48:02 -0700

    challenge 21 done

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var lo = require('lodash');
 
 const port = 8080;
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+let posts = [];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,13 +17,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-app.post("/", function(req, res) {
-  console.log("Posted");
+app.get("/", function(req, res) {
+  res.render("home", {page: "Home", introParagraph: homeStartingContent, posts: posts});
 });
 
-app.get("/", function(req, res) {
-  res.render("home", {page: "Home", introParagraph: homeStartingContent});
+app.get("/posts/:query", function(req, res) {
+  const query = req.params.query;
+  console.log(query);
+  posts.forEach(function(element){
+    if(lo.lowerCase(query) === lo.lowerCase(element.postTitle)){
+      console.log("Match found!");
+      res.render("post", {pageTitle: element.postTitle, postContent: element.postBody})
+    }
+  });
 });
 
 app.get("/about", function(req, res) {
@@ -37,12 +44,14 @@ app.get("/compose", function(req, res) {
   res.render("compose", {page: "Compose"});
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res) {
   const post = {
     postTitle: req.body.blogTitle,
     postBody: req.body.blogBody
   };
-  console.log(post);
+  posts.push(post);
+  // console.log(posts);
+  res.redirect("/");
 })
 
 app.listen(port, function() {
