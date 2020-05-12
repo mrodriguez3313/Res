e98ce59a2ff807e0e8ea32c5561211e943f9a6c1
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-05-12 15:44:32 -0700

    almost done!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -8,7 +9,40 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 const port = 8080;
 
-var items = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+
+// Schema initializations
+var itemsSchema = {
+  name: {
+    type: String,
+    required: true
+  }
+};
+var Item = mongoose.model("Item", itemsSchema);
+
+
+const ListSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+const List = mongoose.model("List", ListSchema);
+
+// Default items
+const listItem1 = new Item({
+  name: "shopping"
+});
+
+const listItem2 = new Item({
+  name:"Hit plus button to add a new item"
+});
+
+const listItem3 = new Item({
+  name: "<-- Hit check box to delete item"
+});
+
+const defaultItems = [listItem1, listItem2, listItem3];
+
+
 
 // GET Methods
 app.get("/", function(req, res){
@@ -16,20 +50,84 @@ app.get("/", function(req, res){
   const options = { weekday: 'long', month: 'long', day: 'numeric'};
   var currentDay = today.toLocaleDateString('en-US', options);
   var day = currentDay;
-  // var weeksdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  // day = weeksdays[currentDay];
-  res.render("index", {kindofDay: day, item:items});
+
+
+  Item.find({}, function(err, foundItems){
+    // console.log(foundItems);
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("Successfully inserted many to Item collection");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("index", {kindofDay: day, listName: "Main", list:foundItems});
+     }
+  })
+});
+
+app.get("/:customList", function(req, res){
+  var today = new Date();
+  const options = { weekday: 'long', month: 'long', day: 'numeric'};
+  var currentDay = today.toLocaleDateString('en-US', options);
+  var day = currentDay;
+  const customListName = req.params.customList;
+  // console.log(customListName);
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err) {
+      if(!foundList){
+        // create new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        console.log("created");
+        res.redirect("/"+customListName);
+      } else {
+        // show existing list
+        console.log("Exists");
+        res.render("index", {kindofDay: day, listName: foundList.name, list: foundList.items})
+      }
+  }
+  });
 });
 
 // POST Methods
 app.post("/", function(req, res){
-  var listItem = req.body.item;
-  items.push(listItem);
-  console.log("button works");
-  console.log("User input: " + listItem);
-  res.redirect("/");
-
+  var itemName = req.body.item;
+  var listName = req.body.list;
+  const item = new Item({
+    name: itemName
+  });
+  if (listName === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+ listName);
+    } );
+  }
+  // console.log("button works");
+  // console.log("User input: " + itemName);
 });
+
+app.post("/delete", function(req, res){
+  const checkedBox = req.body.checkbox;
+  Item.findByIdAndRemove(checkedBox, function(err){
+    if (err){
+      console.log(err);
+    } else {
+      console.log("Successfully deleted " + checkedBox);
+    }
+  });
+  res.redirect("/");
+})
 
 app.listen(port, function(){
   console.log("server running on port " + port)
