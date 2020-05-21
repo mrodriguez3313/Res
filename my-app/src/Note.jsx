bcc738034409f34be782c4002510d7d92c836d5a
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-05-21 15:45:00 -0700

    V2 complete

--- my-app/src/Note.jsx
+++ my-app/src/Note.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Note(){
+function Note(props){
   return <div className="note">
-    <h1> Title of note goes here </h1>
-    <p> This is the body of the note that you are creating. </p>
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
   </div>
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Note from './Note.jsx';
+import notes from './notes.js'
+
 
 ReactDOM.render(
   <div>
     <Header/>
-      <Note/>
+      {notes.map(attributes => (
+        <Note
+          key={attributes.key}
+          title={attributes.title}
+          content={attributes.content}
+        />
+      ))}
     <Footer/>
   </div>,
   document.getElementById('root')
